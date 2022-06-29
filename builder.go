24558ad7310d4416d15162b450b30a5b05be0196
shi.changliang <shi.changliang@zhaopin.com.cn>
2022-06-29 18:19:05 +0800

    refactor:重命名

--- builder.go
+++ builder.go
@@ -1,26 +1,22 @@
 package graphql
 
 import (
-	"fmt"
 	"github.com/ichaly/go-gql/internal/introspection"
-	"github.com/ichaly/go-gql/internal/introspection/kind"
 	"github.com/ichaly/go-gql/types"
 	"reflect"
 )
 
-type SchemaBuilder struct {
+type Builder struct {
 	Name    string
-	enums   map[reflect.Type]*types.Enum
 	objects map[reflect.Type]*types.Object
-	types   map[reflect.Type]*introspection.Type
 }
 
 type BuildOption interface {
-	apply(*SchemaBuilder)
+	apply(*Builder)
 }
 
-func NewSchemaBuilder(options ...BuildOption) *SchemaBuilder {
-	builder := &SchemaBuilder{
+func NewBuilder(options ...BuildOption) *Builder {
+	builder := &Builder{
 		objects: make(map[reflect.Type]*types.Object),
 	}
 	for _, o := range options {
@@ -31,21 +27,21 @@ func NewSchemaBuilder(options ...BuildOption) *SchemaBuilder {
 
 type query struct{}
 
-func (my *SchemaBuilder) Query() *types.Object {
+func (my *Builder) Query() *types.Object {
 	return my.Object("Query", query{})
 }
 
 type mutation struct{}
 
-func (my *SchemaBuilder) Mutation() *types.Object {
+func (my *Builder) Mutation() *types.Object {
 	return my.Object("Mutation", mutation{})
 }
 
 type ObjectOption interface {
-	apply(*SchemaBuilder, *types.Object)
+	apply(*Builder, *types.Object)
 }
 
-func (my *SchemaBuilder) Object(name string, typ interface{}, options ...ObjectOption) *types.Object {
+func (my *Builder) Object(name string, typ interface{}, options ...ObjectOption) *types.Object {
 	if object, ok := my.objects[reflect.TypeOf(typ)]; ok {
 		if reflect.TypeOf(object.Type) != reflect.TypeOf(typ) {
 			panic("re-registered object with different type")
@@ -66,53 +62,7 @@ func (my *SchemaBuilder) Object(name string, typ interface{}, options ...ObjectO
 	return object
 }
 
-func (my *SchemaBuilder) Enum(val interface{}, enumMap interface{}) {
-	typ := reflect.TypeOf(val)
-	if my.enums == nil {
-		my.enums = make(map[reflect.Type]*types.Enum)
-	}
-	eMap, rMap := getEnumMap(enumMap, typ)
-	my.enums[typ] = &types.Enum{Map: eMap, ReverseMap: rMap}
-}
-
-func getEnumMap(enumMap interface{}, typ reflect.Type) (map[string]interface{}, map[interface{}]string) {
-	rMap := make(map[interface{}]string)
-	eMap := make(map[string]interface{})
-	v := reflect.ValueOf(enumMap)
-	if v.Kind() == reflect.Map {
-		for _, key := range v.MapKeys() {
-			val := v.MapIndex(key)
-			valInterface := val.Interface()
-			if reflect.TypeOf(valInterface).Kind() != typ.Kind() {
-				panic("types are not equal")
-			}
-			if key.Kind() == reflect.String {
-				mapVal := reflect.ValueOf(valInterface).Convert(typ)
-				eMap[key.String()] = mapVal.Interface()
-			} else {
-				panic("keys are not strings")
-			}
-		}
-	} else {
-		panic("enum function not passed a map")
-	}
-
-	for key, val := range eMap {
-		rMap[val] = key
-	}
-	return eMap, rMap
-}
-
-func (my *SchemaBuilder) Build() (*introspection.Schema, error) {
-	my.Object("Query", query{})
-	my.Object("Mutation", mutation{})
-	return &introspection.Schema{
-		QueryType:    nil,
-		MutationType: nil,
-	}, nil
-}
-
-func (my *SchemaBuilder) MustBuild() *introspection.Schema {
+func (my *Builder) MustBuild() *introspection.Schema {
 	schema, err := my.Build()
 	if err != nil {
 		panic(err)
@@ -120,30 +70,11 @@ func (my *SchemaBuilder) MustBuild() *introspection.Schema {
 	return schema
 }
 
-func (my *SchemaBuilder) getType(nodeType reflect.Type) (*introspection.Type, error) {
-	// Structs
-	if nodeType.Kind() == reflect.Struct {
-		if err := my.buildStruct(nodeType); err != nil {
-			return nil, err
-		}
-		return &introspection.Type{Kind: kind.NON_NULL, OfType: my.types[nodeType]}, nil
-	}
-	if nodeType.Kind() == reflect.Ptr && nodeType.Elem().Kind() == reflect.Struct {
-		if err := my.buildStruct(nodeType.Elem()); err != nil {
-			return nil, err
-		}
-		return my.types[nodeType.Elem()], nil
-	}
-	if nodeType.Kind() == reflect.Slice {
-		elementType, err := my.getType(nodeType.Elem())
-		if err != nil {
-			return nil, err
-		}
-		return &introspection.Type{Kind: kind.NON_NULL, OfType: &introspection.Type{Kind: kind.LIST, OfType: elementType}}, nil
-	}
-	return nil, fmt.Errorf("bad type %s: should be a scalar, slice, or struct type", nodeType)
-}
-
-func (my *SchemaBuilder) buildStruct(typ reflect.Type) error {
-	return nil
+func (my *Builder) Build() (*introspection.Schema, error) {
+	my.Object("Query", query{})
+	my.Object("Mutation", mutation{})
+	return &introspection.Schema{
+		QueryType:    nil,
+		MutationType: nil,
+	}, nil
 }
